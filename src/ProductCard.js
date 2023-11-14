1521e50552320ef883a28e7338b4904302ad8c58
Isinwa-Deborah <debbyisinwa@gmail.com>
2023-11-13 22:52:15 -0800

    Product-Card with ReactJs

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import image from './assets/melanin.jpg'
 
@@ -9,10 +10,28 @@ const ProductCard = () => {
     const info = "Description";
     const size = "Size:";
     const price = "$188.99";
+    const available = 'Available stock:';
+    const selectedOrder = 'Quantity:';
+
+    const [quantity, setQuantity] = useState(0);
+    const [stock] = useState(10);
+
+    const handleIncrement = (e) => {
+        if (quantity < stock) {
+            setQuantity(quantity + 1);
+        }
+    };
+
+    const handleDecrement = (e) => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1);
+        }
+    };
+
 
     const handleClick = (e) => {
         alert("You liked this product");
-    }
+    };
 
     return (
         <div className="container">
@@ -40,14 +59,25 @@ const ProductCard = () => {
                             from the inside-out, from roots to ends! This luxurious formula contains some of the most effective
                             hair-strengthening and conditioning agents on the market.</p>
                     </div>
-                    <div className='size'>
-                        <h2 style={{ marginTop: '18px', fontFamily: 'Roboto Condensed', color: 'brown' }}>{size}</h2>
+                    <div className='size' style={{ marginTop: '60px', fontFamily: 'Roboto Condensed' }}>
+                        <h2 style={{ color: 'brown' }}>{size}</h2>
                         <p>475ml</p>
+                        <h2 style={{ color: 'brown' }}>{available}</h2>
+                        <p>{stock}</p>
                     </div>
                     <div className='price'>
                         <button>Add to Cart</button>
                         <h2>{price} </h2>
-                        <button onClick={handleClick}>Like</button>
+                        <div>
+                            <button onClick={handleClick}>Like</button>
+                            <button onClick={handleIncrement} style={{ backgroundColor: '#2E8B57' }}>Add Quantity</button>
+                            <button onClick={handleDecrement} style={{ backgroundColor: 'brown' }}>Reduce Quantity</button>
+                        </div>
+                    </div>
+
+                    <div className='choice'>
+                        <h2 style={{ marginTop: '20px', fontFamily: 'Roboto Condensed' }}> {selectedOrder}</h2>
+                        <p>{quantity}</p>
                     </div>
                 </div>
 
